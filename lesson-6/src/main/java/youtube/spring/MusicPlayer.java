1ab76779cf7ab6e7872a47214e07424439afaecb
RuslanBairashev <ruslan.bairashev@gmail.com>
2022-03-30 18:53:57 +0300

    lesson6

---
+++ lesson-6/src/main/java/youtube/spring/MusicPlayer.java
@@ -0,0 +1,38 @@
+package youtube.spring;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+    private String name;
+    private int volume;
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void playMusic() {
+        musicList.forEach(x -> System.out.println("Playing:" + x.getSong()));
+    }
+}
---
+++ lesson-6/src/main/java/youtube/spring/PopMusic.java
@@ -0,0 +1,8 @@
+package youtube.spring;
+
+public class PopMusic implements Music {
+    @Override
+    public String getSong() {
+        return "U Can't Touch This";
+    }
+}
