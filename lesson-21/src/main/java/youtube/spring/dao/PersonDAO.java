48c4b1d5adf15ed6a56b4fde0a201fcfea816bd2
RuslanBairashev <ruslan.bairashev@gmail.com>
2022-04-05 20:27:53 +0300

    lesson-21

---
+++ lesson-21/src/main/java/youtube/spring/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package youtube.spring.dao;
+
+import org.springframework.stereotype.Component;
+import youtube.spring.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(1, "Tom"));
+        people.add(new Person(2, "Bob"));
+        people.add(new Person(3, "Mike"));
+        people.add(new Person(4, "Katy"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return  people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
